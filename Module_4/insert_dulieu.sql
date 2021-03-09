bc8a25166dd384a8f26cc5ed3edf4530ac88acc1
shonepro123 <79887974+shonepro123@users.noreply.github.com>
2021-03-09 07:34:52 +0700

    Add files via upload

---
+++ Module_4/insert_dulieu.sql
@@ -0,0 +1,52 @@
+insert into dmsach values(1,N'Văn Học',' ');
+insert into dmsach values(2,N'Khoa Học',' ');
+insert into dmsach values(3,N'Chính Trị',' ');
+insert into dmsach values(4,N'Tôn Giáo',' ');
+insert into dmsach values(5,N'Thiếu Nhi',' ');
+
+insert into dmdochoi values(1,N'Nhóm 0-3 tuổi',' ');
+insert into dmdochoi values(2,N'Nhóm 4-8 tuổi',' ');
+insert into dmdochoi values(3,N'Nhóm trên 8 tuổi',' ');
+
+insert into dmdungcu values(1,N'Tiểu học',' ');
+insert into dmdungcu values(2,N'Trung học',' ');
+insert into dmdungcu values(3,N'Phổ thông',' ');
+
+insert into loaikhachhang values(1,N'Loại thường');
+insert into loaikhachhang values(2,N'Loại VIP1');
+insert into loaikhachhang values(3,N'Loại VIP2');
+
+insert into nhanvien values(1,N'Trần Nam Trung','1995/11/2',N'Nhân Viên','0345758724','trungtrangf352@gmail.com',N'Quảng Ngãi');
+insert into nhanvien values(2,N'Nguyễn Thị Phúc','1992/11/19',N'Nhân Viên','0463753557','phucnguyen21243@gmail.com',N'Đà Nẵng');
+insert into nhanvien values(3,N'Ngô Minh Tân','1991/10/14',N'Trưởng phòng','0725634634','minhtan34336@gmail.com',N'Đà Nẵng');
+insert into nhanvien values(4,N'Đàm Thuý Vy','1996/8/14',N'Thư ký','03421563422','thuyvy25e@gmail.com',N'Quảng Bình');
+insert into nhanvien values(5,N'Bùi Phước Mạnh Tuyên','1992/11/17',N'Phó Phòng','0353262334','tuyendn5472@gmail.com',N'Đà Nẵng');
+insert into nhanvien values(6,N'Đặng Phược Lộc','1989/6/30',N'Trưởng phòng','0353242523','locphuoc1512@gmail.com',N'Quảng Ngãi');
+insert into nhanvien values(7,N'Phan Thị Chi','1993/3/11',N'Nhân Viên','052632432','chiphan141@gmail.com',N'Đà Nẵng');
+insert into nhanvien values(8,N'Nguyễn Thị Mỹ Dung','1992/14/15',N'Phó Phòng','0365235275','mydungf3dx@gmail.com',N'Quảng Nam');
+insert into nhanvien values(9,N'Trần Đăng Khoa','1990/11/11',N'Trưởng Phòng','03865745646','dangkhoadn@gmail.com',N'Đà Nẵng');
+insert into nhanvien values(10,N'Võ Thị Thắm','1991/3/17',N'Nhân Viên','03678345432','thamqn@gmail.com',N'Quảng Bình');
+
+insert into khachhang values(1,N'Trần Viết Minh Anh','2002/1/2','0363688925','minhanh2021@gmail.com',2);
+insert into khachhang values(2,N'Hoàng Thị An','2004/11/2','0363685525','anhoang432@gmail.com',3);
+insert into khachhang values(3,N'Trần Bá Hoàng','2001/12/10','0363812001','bahoang342@gmail.com',1);
+insert into khachhang values(4,N'Đào Thị Như Hạnh ','2002/1/2','036389440','Nhuhanh421@gmail.com',2);
+insert into khachhang values(5,N'Nguyễn Như Minh','2002/1/10','03669220','nhuminh4231o@gmail.com',2);
+insert into khachhang values(6,N'Phạm Hoàng Kiều','2004/10/11','036445289','Kieupham@gmail.com',1);
+insert into khachhang values(7,N'Nguyễn Viết Minh','2002/12/11','0363698123','minhnguyenxx26@gmail.com',3);
+insert into khachhang values(8,N'Đào Thị Tâm','2002/1/2','0364586358','tamdn241d@gmail.com',1);
+insert into khachhang values(9,N'Trần Nhân Tâm','2003/01/02','0363689990','nhantam3451h@gmail.com',1);
+insert into khachhang values(10,N'Đàm Thị Thuý Tâm','2003/11/02','0355988244','thuytam42@gmail.com',1);
+
+insert into sanphamsach values(1,N'Tuổi trẻ đáng giá bao nhiêu','100','54400','quyển','5','Hội nhà văn','2019','Nguyễn Viết Hoàng','2014/11/27',1);
+insert into sanphamsach values(2,N'Bí mật của nước','100','60000','quyển','2','Văn Học','2019','MARUSAMA','2011/12/07',1);
+insert into sanphamsach values(3,N'Tôi tài giỏi bạn cũng thế','50','116000','quyển','3','Thế giới','2018','Adam Khoo','2014/11/27',2);
+insert into sanphamsach values(4,N'Giải mã siêu trí nhớ','20','88000','quyển','2','Thế giới','2019','Mai Tường Vân','2016/02/16',2);
+insert into sanphamsach values(5,N'Kẻ trộm sách','100','94400','quyển','1','Thế giới','2019','Hoàng Minh','1999/06/07',1);
+insert into sanphamsach values(6,N'Kính Vạn Hoa','188','167200','quyển','4','Thế giới','2018','Nguyễn Ngọc Ánh','2020/12/11',1);
+insert into sanphamsach values(7,N'Cha giàu cha nghèo','55','187000','quyển','1','Hà Nội','2020','Nguyễn Minh Phong','2012/10/13',1);
+insert into sanphamsach values(8,N'Chiến dịch đại dương xanh','66','116000','quyển','1','Hà Nội','2020','Thomas Brist','2016/9/22',1);
+insert into sanphamsach values(9,N'1 vạn câu hỏi vì sao','63','72000','quyển','1','Thế giới','2018','Heley Phạm','2018/10/30',1);
+insert into sanphamsach values(10,N'Suối Nguồn','30','128000','quyển','5','Thế Giới','2020','jenny','2020/04/11',1);
+
+insert into sanphamdochoi values(1,N'Chiến lược học nhanh','100','650000','đ','5','Kim Đồng','2019','Phạm Tuân','2019/11/27',1);
